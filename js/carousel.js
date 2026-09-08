3f004119511d7c86de67c254330492077c6f9fc9
Emilio Rojo Núñez <emi.rojo@icloud.com>
2026-09-08 19:34:24 -0300

    Push inicial de ConFun

---
+++ js/carousel.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+      let slideInterval;
+
+      document.addEventListener('DOMContentLoaded', () => {
+        startInterval();
+      });
+
+      function startInterval() {
+        slideInterval = setInterval(() => moveSlide(1), 7000);
+      }
+
+      function moveSlide(i){
+        const slides = document.querySelectorAll('.carousel-image');
+        slides[currentSlide].classList.remove('active');
+        currentSlide += i;
+
+        if (currentSlide >= slides.length) { currentSlide = 0; }
+        if (currentSlide < 0) { currentSlide = slides.length - 1;}
+
+        slides[currentSlide].classList.add('active');
+  
+        clearInterval(slideInterval);
+        startInterval();
+      }
